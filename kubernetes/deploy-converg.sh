68285d6a0c286aea930d8afa22be548e739443d6
Jey Puget Gil <LinkJerome@gmail.com>
2024-08-27 16:48:32 +0200

    Created a deployment script (workflow like)

---
+++ kubernetes/deploy-converg.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+execute_job_and_wait() {
+    job_name=$1
+    job_completed=false
+    while ! $job_completed; do
+        kubectl wait --for=condition=complete job/$job_name --timeout=30s --namespace=ud-evolution
+        if [[ $? -eq 0 ]]; then
+            echo "Job $job_name completed successfully"
+            job_completed=true
+        fi
+        echo "Waiting for job $job_name to complete"
+    done
+}
+
+# Preparing deployments on Pagoda
+export KUBECONFIG=~/.kube/config-pagoda3.yaml
+
+echo "Make sure that the KUBECONFIG file is up to date."
+
+kubectl apply -f databases --namespace=ud-evolution
+kubectl apply -f conver-g --namespace=ud-evolution
+
+# Handmade workflow
+kubectl apply -f dataset/dataset-pvc.yml --namespace=ud-evolution
+
+kubectl apply -f dataset/generate-dataset.yml --namespace=ud-evolution
+execute_job_and_wait dataset-generation-job
+
+kubectl apply -f dataset/transform-dataset.yml --namespace=ud-evolution
+execute_job_and_wait dataset-transformer-job
+
+kubectl apply -f dataset/import-dataset.yml --namespace=ud-evolution
+execute_job_and_wait dataset-importer-job
